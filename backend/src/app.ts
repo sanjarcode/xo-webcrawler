0e823c3dd5134e8113c5069b449d9559d9d1fd77
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-05-20 18:13:11 +0530

    Capture CLI params

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,37 +1,54 @@
-
-import { UrlLoaderService } from './services/url-loader.service.js'
-import { Command } from 'commander'
+import { UrlLoaderService } from "./services/url-loader.service.js";
+import { Command } from "commander";
 
 interface AppParameters {
-  url: string
+  url: string;
+  word: string;
+  level: number;
 }
 
-export const DEFAULT_URL = 'https://www.kayako.com/'
+export const DEFAULT_URL = "https://www.kayako.com/";
+export const DEFAULT_WORD = "kayako";
+export const DEFAULT_LEVEL = "1";
 
 export class App {
   /* istanbul ignore next */
-  constructor (private readonly urlLoader: UrlLoaderService, private readonly command = new Command()) {
+  constructor(
+    private readonly urlLoader: UrlLoaderService,
+    private readonly command = new Command()
+  ) {}
+
+  async run(): Promise<void> {
+    const appParameters = this.parseCli();
+
+    await this.process(appParameters);
   }
 
-  async run (): Promise<void> {
-    const appParameters = this.parseCli()
-
-    await this.process(appParameters)
+  async process(appParameters: AppParameters): Promise<void> {
+    const extractedText = await this.urlLoader.loadUrlTextAndLinks(
+      appParameters.url
+    );
+    const count = (
+      extractedText.text.toLocaleLowerCase().match(/kayako/gi) ?? []
+    ).length;
+    console.log(
+      `Found ${count} instances of '${appParameters.word}' in the body of the page '${appParameters.url}', at max level ${appParameters.level}`
+    );
   }
 
-  async process (appParameters: AppParameters): Promise<void> {
-    const extractedText = await this.urlLoader.loadUrlTextAndLinks(appParameters.url)
-    const count = (extractedText.text.toLocaleLowerCase().match(/kayako/ig) ?? []).length
-    console.log(`Found ${count} instances of 'kayako' in the body of the page`)
-  }
+  parseCli(argv: readonly string[] = process.argv): AppParameters {
+    this.command.requiredOption("-u, --url <url>", "URL to load", DEFAULT_URL);
+    this.command.option("-w, --word <word>", "Word to match", DEFAULT_WORD);
 
-  parseCli (argv: readonly string[] = process.argv): AppParameters {
-    this.command
-      .requiredOption('-u, --url <url>', 'URL to load', DEFAULT_URL)
+    this.command.option(
+      "-l, --level <level>",
+      "Level to look at",
+      DEFAULT_LEVEL
+    );
 
-    this.command.parse(argv)
-    const options = this.command.opts()
-
-    return { url: options.url }
+    this.command.parse(argv);
+    const options = this.command.opts();
+    console.log({ url: options.url, word: options.word, level: options.level });
+    return { url: options.url, word: options.word, level: options.level };
   }
 }
